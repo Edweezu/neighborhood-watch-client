a7dcfa550e64a9aff5f530db8e876ede194bbaae
Edwin Qiu <edwinqiu93@gmail.com>
2019-10-19 04:07:31 -0700

    changed API endpoint to prod

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports =  {
     PORT: process.env.PORT || 8000,
-    API_ENDPOINT: "http://localhost:8000/api",
-    // API_ENDPOINT: "https://sleepy-atoll-56434.herokuapp.com/api",
+    // API_ENDPOINT: "http://localhost:8000/api",
+    API_ENDPOINT: "https://sleepy-atoll-56434.herokuapp.com/api",
     TOKEN_KEY: 'neighborhood-client-auth-token'
 }
